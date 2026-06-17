f68c531213c0842ab3f11964484eba4d68275b0a
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-06-17 18:00:56 +0530

    add components and context for player functionality; implement album and song displays, navigation, and asset management

--- spotify-clone/src/components/sidebar.jsx
+++ spotify-clone/src/components/sidebar.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { assets } from './../assets/frontend-assets/assets';
 
-const Sidebar = () => {
-  return (
-    <div className="w-64 bg-black text-white h-screen p-5">
-      <div className="">
-        <h1 className="text-3xl font-bold text-green-500 mb-10">
-        <img className="logo w-10  bg-black text-white h-10" src="https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_640.jpg"/>
-        Spotify
-      </h1>
-      </div>
-      
-      <ul className="space-y-5">
-        <li className="cursor-pointer hover:text-green-500">
-          Home
-        </li>
+function Sidebar() {
+    const navigate = useNavigate();
+    return (
+        <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+            <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+                <div onClick={() => navigate("/")} className="flex items-center gap-3 pl-8 cursor-pointer">
+                    <img className='w-6' src={assets.home_icon} alt="home icon" />
+                    <p className='font-bold'>Home</p>
+                </div>
+                <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                    <img className='w-6' src={assets.search_icon} alt="search icon" />
+                    <p className='font-bold'>Search</p>
+                </div>
+            </div>
+            <div className="bg-[#121212] h-[85%] rounded">
+                <div className="p-4 flex items-center justify-between">
+                    <div className="flex items-center gap-3">
+                        <img className='w-8' src={assets.stack_icon} alt="stack_icon" />
+                        <p className="font-semibold">Your Library</p>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        <img className='w-5' src={assets.arrow_icon} alt="arrow_icon" />
+                        <img className='w-5' src={assets.plus_icon} alt="plus_icon" />
+                    </div>
+                </div>
+                <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+                    <h1>Create your first playlist</h1>
+                    <p className='font-light'>It&apos;s easy we will help you</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                </div>
+                <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start mt-4 gap-1 pl-4">
+                    <h1>Let&apos;s findsome podcasts to follow</h1>
+                    <p className='font-light'>We&apos;ll keep you update on new episodes</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-        <li className="cursor-pointer hover:text-green-500">
-          Search
-        </li>
-
-        <li className="cursor-pointer hover:text-green-500">
-          Your Library
-        </li>
-      </ul>
-    </div>
-  );
-};
-
-export default Sidebar;
+export default Sidebar
