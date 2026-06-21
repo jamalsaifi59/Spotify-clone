87bfb26efc916a7d073ebe1a7e1afa4afe17e9dc
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-06-21 17:46:15 +0530

    error handling and code structure in song controller and routes

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,10 +1,29 @@
-import { addSong, listSong, removeSong } from "../controllers/songs.controller.js";
-import {Router} from "express";
+import {
+  addSong,
+  listSong,
+  removeSong,
+} from "../controllers/songs.controller.js";
+import { Router } from "express";
 import upload from "../middleware/multer.middleware.js";
 
 const songRouter = Router();
 
-songRouter.route('/add').post (upload.fields([{ name: 'image', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), addSong);
-songRouter.route('/list').get(listSong);
-songRouter.route('/remove/:id').delete (removeSong)
+songRouter.route("/add").post(
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "audio", maxCount: 1 },
+  ]),
+  addSong,
+);
+// songRouter.post(
+//   '/add',
+//   upload.any(),
+//   (req, res) => {
+//     console.log("files : ",req.files);
+//     console.log("body : ", req.body);
+//     res.json({ success: true });
+//   }
+// );
+songRouter.route("/list").get(listSong);
+songRouter.route("/remove/:id").delete(removeSong);
 export default songRouter;
