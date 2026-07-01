a07c78e558562dc043348bdb0721ab53966af32d
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-07-01 17:37:55 +0530

    nav bar name change

--- spotify-clone/src/components/navbar.jsx
+++ spotify-clone/src/components/navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
                 <div className="flex items-center gap-4">
                     <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
                     <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
-                    <p className='bg-sky-400 text-black w-7 h-7 rounded-full flex items-center justify-center'>N</p>
+                    <p className='bg-sky-400 text-black w-7 h-7 rounded-full flex items-center justify-center'>JS</p>
                 </div>
             </div>
             <div className="flex items-center gap-2 mt-4">
