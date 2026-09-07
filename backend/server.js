91061565142d93653203d288145476602a012ce5
jamal saifi <jamalsaifi379@gmail.com>
2026-09-07 12:44:46 +0530

    user route change

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cors())
 
 //initializing routes
-app.use("/api/users",userRoutes)
+app.use("/api/user",userRoutes)
 app.use("/api/song", songRouter)
 app.use("/api/album", albumRouter)
 app.get('/', (req, res) => res.send("Spotify Backend Running"));
